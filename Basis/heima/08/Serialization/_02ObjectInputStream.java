802d13c523113cf9b7de8540c646394816e34abb
Asianwyz <asiawyz@icloud.com>
2019-04-09 22:05:05 +0800

    Serialization

---
+++ Basis/heima/08/Serialization/_02ObjectInputStream.java
@@ -0,0 +1,41 @@
+package heima.Serialization;
+/*
+    java.io.ObjectInputStream extends InputStream
+    ObjectInputStream:对象的反序列化流
+    作用:把文件中保存的对象,以流的方式读取出来使用
+
+    构造方法:
+        ObjectInputStream(InputStream in) 创建从指定 InputStream 读取的 ObjectInputStream。
+        参数:
+            InputStream in:字节输入流
+    特有的成员方法:
+        Object readObject() 从 ObjectInputStream 读取对象。
+
+    使用步骤:
+        1.创建ObjectInputStream对象,构造方法中传递字节输入流
+        2.使用ObjectInputStream对象中的方法readObject读取保存对象的文件
+        3.释放资源
+        4.使用读取出来的对象(打印)
+
+     readObject方法声明抛出了ClassNotFoundException(class文件找不到异常)
+     当不存在对象的class文件时抛出此异常
+     反序列化的前提:
+        1.类必须实现Serializable
+        2.必须存在类对应的class文件
+ */
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.Scanner;
+
+public class _02ObjectInputStream {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("src\\heima\\Serialization\\person.txt"));
+        Object o = ois.readObject();
+        System.out.println(o);
+        Person person = (Person) o;
+        System.out.println(person.getName() + " " + person.getAge());
+        Scanner in = new Scanner(System.in);
+    }
+}
--- Readme.txt
+++ Readme.txt
@@ -1,1 +1,23 @@
-Asia的Java学习之路
+﻿Asia的Java学习之路
+Basis
+    -heima        
+        01 ----->  ArrayList Scanner			ArrayList类 Scanner类
+        02 ----->  String 				字符串
+        03 ----->  Collection    Iterator    Generic  		集合，迭代，泛型
+        04 ----->  List LinkedList  Set  Collections  Map	List	链表List，Set，HashSet，Collections工具类，Map
+        05 ----->  Exception                                              	异常
+        06 ----->  Thread                       			线程
+        07 ----->  File					文件类
+        08 ----->  IO  Buffer  Serialization  PrintStream	IO	简单输入输出流，缓冲流，序列化，打印流
+
+    -xtzx
+        学堂在线Java程序设计编程题
+        Unit1
+        Unit2
+
+Job
+    -huawei
+    -tengxun
+    -wangyi
+
+Projects
