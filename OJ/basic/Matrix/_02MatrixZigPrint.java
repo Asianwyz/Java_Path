cae9066523e0c2a5033711162aa521badd342cb1
Asianwyz <asiawyz@icloud.com>
2019-05-06 22:13:22 +0800

    Zig

---
+++ OJ/basic/Matrix/_02MatrixZigPrint.java
@@ -0,0 +1,24 @@
+package basic.Matrix;
+
+/**
+ * 之” 字形打印矩阵
+ * 【 题目】 给定一个矩阵matrix， 按照“之” 字形的方式打印这
+ * 个矩阵， 例如：
+ * 1  2  3  4
+ * 5  6  7  8
+ * 9 10 11 12
+ * “之” 字形打印的结果为： 1， 2， 5， 9， 6， 3， 4， 7， 10， 11，8， 12
+ * 【 要求】 额外空间复杂度为O(1)
+ */
+
+public class _02MatrixZigPrint {
+    public static void main(String[] args) {
+        int[][] nums = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+        boolean flag = false;
+        int row1 = 0, col1 = 0;
+        int row2 = 0, col2 = 0;
+        while
+
+    }
+
+}
